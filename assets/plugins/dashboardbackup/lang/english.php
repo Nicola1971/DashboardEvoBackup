39fe98e0b1b8c51bb76065eeb4f07b974fa5e5eb
Nicola <modxit@modx.it>
2017-10-24 19:57:13 +0200

    ok

--- assets/plugins/dashboardbackup/lang/english.php
+++ assets/plugins/dashboardbackup/lang/english.php
@@ -27,3 +27,4 @@ $_lang['backup_filesize'] = 'File size';
 $_lang['backup_filedate'] = 'Date';
 $_lang['backup_file_options'] = 'Options';
 $_lang['last_db_backup'] = 'Last DataBase Backup';
+$_lang['last_zip_backup'] = 'Last Files Backup';
